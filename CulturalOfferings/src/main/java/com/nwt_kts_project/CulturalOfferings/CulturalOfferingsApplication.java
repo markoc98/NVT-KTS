ffb54b3ac248d6a9a4d645b8d56577e9d2f2484e
unknown <marko.cvijanovic.1998@gmail.com>
2020-11-21 20:04:12 +0100

    initial commit

---
+++ CulturalOfferings/src/main/java/com/nwt_kts_project/CulturalOfferings/CulturalOfferingsApplication.java
@@ -0,0 +1,13 @@
+package com.nwt_kts_project.CulturalOfferings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalOfferingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalOfferingsApplication.class, args);
+	}
+
+}
---
+++ CulturalOfferings/src/test/java/com/nwt_kts_project/CulturalOfferings/CulturalOfferingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwt_kts_project.CulturalOfferings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalOfferingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
