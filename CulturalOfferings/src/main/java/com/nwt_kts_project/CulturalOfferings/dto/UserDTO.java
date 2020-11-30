0b491a56d761ca69a5e78414800f08936769b2d0
Mihailo Bubnjevic <mihabubnjevic@gmail.com>
2020-11-30 01:07:02 +0100

    Upotpunjen user service i user repository, napravljen UserDTO

---
+++ CulturalOfferings/src/main/java/com/nwt_kts_project/CulturalOfferings/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package com.nwt_kts_project.CulturalOfferings.dto;
+
+public class UserDTO {
+}
--- CulturalOfferings/src/main/java/com/nwt_kts_project/CulturalOfferings/service/UserService.java
+++ CulturalOfferings/src/main/java/com/nwt_kts_project/CulturalOfferings/service/UserService.java
@@ -1,34 +1,54 @@
 package com.nwt_kts_project.CulturalOfferings.service;
 
 import com.nwt_kts_project.CulturalOfferings.model.User;
+import com.nwt_kts_project.CulturalOfferings.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class UserService implements ServiceInterface<User> {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public List<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User findOne(Long id) {
-        return null;
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
-    public User create(User entity) throws Exception {
-        return null;
+    public User create(User user) throws Exception {
+        if(userRepository.findByEmail(user.getEmail()) != null){
+            throw new Exception("User with given email address already exists");
+        }
+        if(userRepository.findByUsername(user.getUsername()) != null){
+            throw new Exception("User with same username already exists");
+        }
+        return userRepository.save(user);
     }
 
     @Override
-    public User update(User entity, Long id) throws Exception {
-        return null;
+    public User update(User user, Long id) throws Exception {
+        User existingUser =  userRepository.findById(id).orElse(null);
+        if(existingUser == null){
+            throw new Exception("User with given id doesn't exist");
+        }
+        existingUser.setPassword(user.getPassword());
+        return userRepository.save(existingUser);
     }
 
     @Override
     public void delete(Long id) throws Exception {
-
+        User existingUser = userRepository.findById(id).orElse(null);
+        if(existingUser == null){
+            throw new Exception("User with given id doesn't exist");
+        }
+        userRepository.delete(existingUser);
     }
 }
