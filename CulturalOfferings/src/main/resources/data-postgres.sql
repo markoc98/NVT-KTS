7c40c2e14037f9b6b0987b360e3b3284d3a8019e
marinagusa <marina.gusa.99@gmail.com>
2021-01-27 20:51:03 +0100

    e2e za cult off tabelu pocetak

--- CulturalOfferings/src/main/resources/data-postgres.sql
+++ CulturalOfferings/src/main/resources/data-postgres.sql
@@ -63,7 +63,6 @@ INSERT INTO cultural_offerings (location, name, description, rating,category_id,
 INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,1);
 INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,2);
 INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,3);
-INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,4);
 INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,5);
 INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,6);
 INSERT INTO user_cultural_offering (user_id, cultural_offering_id) VALUES (2,7);
