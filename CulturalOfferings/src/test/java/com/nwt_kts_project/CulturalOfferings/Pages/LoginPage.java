1e8078c2351b4a148eb721e3c582d7c062164e80
Mihailo Bubnjevic <mihabubnjevic@gmail.com>
2021-01-27 11:26:23 +0100

    e2e test login page

--- CulturalOfferings/src/test/java/com/nwt_kts_project/CulturalOfferings/Pages/LoginPage.java
+++ CulturalOfferings/src/test/java/com/nwt_kts_project/CulturalOfferings/Pages/LoginPage.java
@@ -1,4 +1,59 @@
 package com.nwt_kts_project.CulturalOfferings.Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 public class LoginPage {
+
+    private WebDriver driver;
+
+    private LoginPage loginPage;
+
+    @FindBy(id = "username")
+    private WebElement email;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "submit")
+    private WebElement loginBtn;
+
+    public LoginPage() {
+        super();
+    }
+
+    public LoginPage(WebDriver driver) {
+        super();
+        this.driver = driver;
+    }
+
+    public WebElement getEmail() {
+        return email;
+    }
+
+    public WebElement getPassword() {
+        return password;
+    }
+
+    public WebElement getLoginBtn() {
+        return loginBtn;
+    }
+
+    public void ensureIsDisplayedUsername() {
+        (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.id("username")));
+    }
+    public void ensureIsClickableLoginBtn() {
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.id("submit")));
+    }
+
+    public void ensureIsNotVisibleUsername() {
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.invisibilityOfElementLocated(By.id("username")));
+    }
+
+
 }
---
+++ CulturalOfferings/src/test/java/com/nwt_kts_project/CulturalOfferings/e2e/LoginE2ETest.java
@@ -0,0 +1,11 @@
+package com.nwt_kts_project.CulturalOfferings.e2e;
+
+import com.nwt_kts_project.CulturalOfferings.Pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+public class LoginE2ETest {
+
+    private WebDriver driver;
+
+    private LoginPage loginPage;
+}
