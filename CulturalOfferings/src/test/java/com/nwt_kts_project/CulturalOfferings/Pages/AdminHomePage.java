4c90097b8146bec7edd14bb9c1e366b9e8493212
marinagusa <marina.gusa.99@gmail.com>
2021-01-27 03:19:09 +0100

    napravljen page za hp admina za e2e

---
+++ CulturalOfferings/src/test/java/com/nwt_kts_project/CulturalOfferings/Pages/AdminHomePage.java
@@ -0,0 +1,23 @@
+package com.nwt_kts_project.CulturalOfferings.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdminHomePage {
+	
+    private WebDriver driver;
+
+	public AdminHomePage() {
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+    
+    
+
+
+}
